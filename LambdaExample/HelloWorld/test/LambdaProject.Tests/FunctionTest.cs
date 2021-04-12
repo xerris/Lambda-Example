5b3f9ea4cffdd2bd4a7a8c44c620922792b132c8
Jason Wiker <jason.wiker@gmail.com>
2021-04-12 15:25:30 -0600

    Add stub lambdas to start

---
+++ LambdaExample/HelloWorld/test/LambdaProject.Tests/FunctionTest.cs
@@ -0,0 +1,67 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Amazon.Lambda.S3Events;
+using Xunit;
+using Amazon.Lambda.TestUtilities;
+using Amazon.Lambda.SQSEvents;
+using Amazon.S3.Util;
+using HelloWorld;
+
+namespace HelloWorld.Tests
+{
+    public class FunctionTest
+    {
+        [Fact]
+        public async Task TestSQSEventLambdaFunction()
+        {
+            var sqsEvent = new SQSEvent
+            {
+                Records = new List<SQSEvent.SQSMessage>
+                {
+                    new SQSEvent.SQSMessage
+                    {
+                        Body = "foobar"
+                    }
+                }
+            };
+
+            var logger = new TestLambdaLogger();
+            var context = new TestLambdaContext
+            {
+                Logger = logger
+            };
+
+            var function = new Function();
+            await function.FunctionHandler(sqsEvent, context);
+
+            Assert.Contains("Processed message foobar", logger.Buffer.ToString());
+        }
+        
+        [Fact]
+        public async Task Tests3EventLambdaFunction()
+        {
+            var sqsEvent = new S3Event()
+            {
+                Records = new List<S3Event.S3EventNotificationRecord>()
+                
+            };
+            var notificationRecord = new S3EventNotification.S3EventNotificationRecord();
+            notificationRecord.S3.Bucket.Name = "gary";
+            notificationRecord.S3.Object.Key = "test.csv";
+            sqsEvent.Records.Append(notificationRecord);
+
+            var logger = new TestLambdaLogger();
+            var context = new TestLambdaContext
+            {
+                Logger = logger
+            };
+
+            var function = new Function();
+            await function.S3FunctionHandler(sqsEvent, context);
+
+            Assert.Contains("Processed message foobar", logger.Buffer.ToString());
+        }
+    }
+}
